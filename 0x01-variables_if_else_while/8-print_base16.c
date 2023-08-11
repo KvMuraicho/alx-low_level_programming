e4643df23ee4cc67c7759f929b4f22383d17ad7f
KvMuraicho <muraichokelly@gmail.com>
2023-08-11 13:58:40 -0700

    m

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,15 +13,17 @@ int main(void)
 	for (i = 0; i < 16; i++)
 	
 	{
-        if (i < 10)
-            c = i + '0';
-        else
-            c = i - 10 + 'a';
-
-        putchar(c);
-    }
-    putchar('\n');
-
-    return (0);
+		if (i < 10)
+		c = i + '0';
+		
+		else
+			c = i - 10 + 'a';
+		
+		putchar(c);
+	
+	}
+	putchar('\n');
+	
+	return (0);
 }
 
