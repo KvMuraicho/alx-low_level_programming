156e178247854aa6fe8a7be5d1ccfd21f32cb968
KvMuraicho <muraichokelly@gmail.com>
2023-08-11 10:47:25 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
     {
         if (c != 'e' && c != 'q')
         {
@@ -20,4 +20,3 @@ int main(void)
 
     return (0);
 }
-
