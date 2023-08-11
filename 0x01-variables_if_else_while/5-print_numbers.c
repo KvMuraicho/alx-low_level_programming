ca5de9185825524889870bba64474b1ad69374cc
KvMuraicho <muraichokelly@gmail.com>
2023-08-11 12:32:24 -0700

    m

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,27 @@
+/*
+ * File: 4-print_alphabt.c
+ * 
+ * Return:  Always 0.
+ */
+
 #include <stdio.h>
+
 /**
- * main - prints all single digit numbers of base 10 starting from 0
+ * main - Prints the alphabet in lowercase, except for q and e.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char num;
-	
-	for (num = '0'; num <= '9'; num++)
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(num);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
