982c889f517f83c915683471f0cdb446728733be
KvMuraicho <muraichokelly@gmail.com>
2023-08-11 12:47:06 -0700

    m

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
 /*
- * File: 4-print_alphabt.c
- *  
+ * File: 4-print_alphabt.c  
  */
 
 #include <stdio.h>
@@ -13,7 +12,7 @@
 int main(void)
 {
 	char num;
-	
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
