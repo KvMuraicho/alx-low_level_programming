77aa62341e40249c6cd41f42fb957a6ac523bfbb
KvMuraicho <muraichokelly@gmail.com>
2023-08-15 12:42:04 -0700

    commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned long a = 1, b = 2, next, sum = 2;  /* We initialize sum to 2 because the second Fibonacci term is 2 and even. */
+
+    while (1)
+    {
+        next = a + b;
+
+        if (next > 4000000)  /* If the next Fibonacci number exceeds 4,000,000, break out of the loop. */
+            break;
+
+        if (next % 2 == 0)  /* If the Fibonacci number is even, add to sum. */
+            sum += next;
+
+        /* Move to the next Fibonacci terms. */
+        a = b;
+        b = next;
+    }
+
+    printf("%lu\n", sum);  /* Print the sum. */
+    return 0;
+}
+
