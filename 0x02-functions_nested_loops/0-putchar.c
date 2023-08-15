7d91d54b07570945d7723fe014b9b3c155ab8081
KvMuraicho <muraichokelly@gmail.com>
2023-08-15 04:20:34 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void) {
-    printf("_putchar\n");
+    char str[] = "_putchar";
+    for(int i = 0; str[i] != '\0'; i++) {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+
     return 0;
 }
-
