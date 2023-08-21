86cc676bec31b83d1143bc87559e741eac147945
KvMuraicho <muraichokelly@gmail.com>
2023-08-21 03:10:40 -0700

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,15 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: pointer to first value
- * @b: pointer to second value
+ * a: pointer to first value
+ * b: pointer to second value
  */
 
-void swap(int *a, int *b) {
-	int temp = *a;  
-	*a = *b;       
-       	*b = temp;     
-}
+void swap_int(int *a, int *b)
+{
+	int c;
 
+	c = *a;
+	*a = *b;
+	*b = c;
+}
