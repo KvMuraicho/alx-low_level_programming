8995b724d5f3808740226ca009ff6d2e004a9d12
KvMuraicho <muraichokelly@gmail.com>
2023-08-21 13:51:13 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,30 +10,32 @@
 int _atoi(char *s)
 {
 	int i = 0, sign = 1, num_started = 0, res = 0;
-       
+
 	while (s[i])
-{
-	if (s[i] == '-')            
-		sign = sign * -1;
-       
-	if (s[i] >= '0' && s[i] <= '9') 
 	{
-	       	if(res > (INT_MAX - (s[i] - '0')) / 10)
+		if (s[i] == '-')            
+			sign = sign * -1;
+
+		if ((s[i] >= '0') && (s[i] <= '9'))
 		{
-		       	if(sign == 1)
-			       	return INT_MAX;
-		       	else
-			       	return INT_MIN;
+			if (res > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+			num_started = 1;
+			res = res * 10 + (s[i] - '0');
 		}
-	       
-		num_started = 1;
-	       	res = res * 10 + (s[i] - '0');
-       	}
-       	else if (num_started)      
-	       	break;
-       	i++;
+		else if (num_started)
+			break;
+
+		i++;
+	}
+
+	return (sign * res);
 }
-return sign * res;
-}
+
 
 
