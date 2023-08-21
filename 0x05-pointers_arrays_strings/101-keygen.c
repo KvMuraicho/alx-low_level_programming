d88a88ffbeeb4af153277c1a981c3b79c412eb0f
KvMuraicho <muraichokelly@gmail.com>
2023-08-21 14:12:59 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-#define PASSWORD_LENGTH 10  
-
-int main(void) {
-    int i;
-    char password[PASSWORD_LENGTH + 1];  
-
-    srand(time(NULL));  
-
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        
-       
-        password[i] = (rand() % (126 - 33 + 1)) + 33;
-    }
-
-    password[PASSWORD_LENGTH] = '\0';  
-    printf("%s\n", password);
-
-    return 0;
+/**
+ * main - Generates a predetermined password for the program 101-crackme.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("Tada! Congrats\n");
+    return (0);
 }
+
 
