7cf5aa2b9ed167fe62b49543b8eb329864daf44d
KvMuraicho <muraichokelly@gmail.com>
2023-08-24 15:03:37 -0700

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rot13 - encodes a string using rot13
  * @s: The string to encode.
@@ -7,20 +6,21 @@
  * Return: The encoded string.
  */
 char *rot13(char *s) {
-    int i = 0;
-    int j;
-    char plain[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char cipher[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-    while (s[i]) {
-        for (j = 0; j < 52; j++) {
-            if (s[i] == plain[j]) {
-                s[i] = cipher[j];
-                break;  
-            }
-        }
-        i++;
-    }
-    return s;
+       	int i = 0;
+       	int j;
+       	char plain[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+       	char cipher[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+       
+	while (s[i]) {
+	       	for (j = 0; j < 52; j++) {
+		       	if (s[i] == plain[j]) {
+			       	s[i] = cipher[j];
+			      
+				break;  
+		       	}
+		}
+	       	i++;
+       	}
+       	return s;
 }
 
