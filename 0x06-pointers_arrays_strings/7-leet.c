f9e89db4a2d1d4abe40cab6e8beb52f69776492d
KvMuraicho <muraichokelly@gmail.com>
2023-08-24 14:46:52 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,19 @@
  * Return: The encoded string.
  */
 char *leet(char *s) {
-       	int i, j;
-       	char letters[] = "aAeEoOtTlL";
-       	char numbers[] = "43007110";
-       
-	for (i = 0; s[i]; i++) {
-	       	for (j = 0; letters[j]; j++) {
-		       	if (s[i] == letters[j]) {
-			       	s[i] = numbers[j / 2];
-			       	break;
-		       	}
-	       	}
-       	}
-       	return s;
+    int i, j;
+    char letters[] = "aAeEoOtTlL";
+    char numbers[] = "43007110";
+
+    for (i = 0; s[i]; i++) {
+        for (j = 0; letters[j]; j++) {
+            if (s[i] == letters[j]) {
+                s[i] = numbers[j / 2];
+                break;
+            }
+        }
+    }
+
+    return s;
 }
+
