0a5efbc866496a4146c971f5203019bfa7032b15
KvMuraicho <muraichokelly@gmail.com>
2023-08-24 15:48:38 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,13 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
- */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
@@ -20,23 +13,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		biggest = a_len;
 	else
 		biggest = b_len;
-	if ((biggest + 1) >= size_r)
+
+	if ((biggest + 2) > size_r) 
 		return (0);
 	r[biggest + 1] = '\0';
-	
+
+	a_len--;
+	b_len--;
+
 	while (biggest >= 0)
 	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-		
+		a = (a_len >= 0) ? (n1[a_len] - '0') : 0;  
+		b = (b_len >= 0) ? (n2[b_len] - '0') : 0;  
+
+		sum = a + b + carry;
+
 		if (sum > 9)
 		{
 			carry = sum / 10;
@@ -52,8 +43,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		b_len--;
 		biggest--;
 	}
-	if (*(r) != 0)
+
+	if (carry)  
+		return (0);
+
+	if (*(r) != '0') 
 		return (r);
 	else
 		return (r + 1);
 }
+
