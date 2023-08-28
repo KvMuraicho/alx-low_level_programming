ef68564af8be59bbc2683926e16a4e05f8822653
KvMuraicho <muraichokelly@gmail.com>
2023-08-28 11:10:40 -0700

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
