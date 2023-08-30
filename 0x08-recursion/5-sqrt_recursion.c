46271cbb459d9e3c41e7fd1affdba99a94a6f2e0
KvMuraicho <muraichokelly@gmail.com>
2023-08-30 13:19:32 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,22 +3,21 @@
  * _sqrt_check - Checks for the square root.
  * @n: The number to find the square root of.
  * @i: The current divisor to check.
- *
+ * 
  * Return: The square root if found, -1 otherwise.
  */
 int _sqrt_check(int n, int i)
 {
-    if (i * i == n)
-    {
-        return (i);
-    }
-    if (i * i > n)
-    {
-        return (-1);
-    }
-    return _sqrt_check(n, i + 1);
+       	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+	       	return (-1);
+	}
+       	return _sqrt_check(n, i + 1);
 }
-
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to find the square root of.
@@ -27,10 +26,10 @@ int _sqrt_check(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    return _sqrt_check(n, 1);
+       	if (n < 0)
+	{
+		return (-1);
+	}
+       	return _sqrt_check(n, 1);
 }
 
