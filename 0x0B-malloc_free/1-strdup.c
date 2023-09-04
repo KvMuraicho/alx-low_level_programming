17c449b7cf757ef4a1e2c355b578e88311057d2f
KvMuraicho <muraichokelly@gmail.com>
2023-09-04 04:28:03 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,22 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int i, len = 0;
+	char *duplicate;
+	unsigned int i, len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
-    duplicate = (char *)malloc(len + 1);
-    if (duplicate == NULL)
-        return (NULL);
+	duplicate = (char *)malloc(len + 1);
+	if (duplicate == NULL)
+		return (NULL);
 
-    for (i = 0; i <= len; i++)
-        duplicate[i] = str[i];
+	for (i = 0; i <= len; i++)
+		duplicate[i] = str[i];
 
-    return (duplicate);
+	return (duplicate);
 }
 
