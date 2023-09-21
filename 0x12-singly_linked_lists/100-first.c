afd404a5c8ac833d5960a9f4889b7f6a40850dd0
KvMuraicho <muraichokelly@gmail.com>
2023-09-21 08:26:50 -0700

    created 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * premain - Function to be executed before main
+ * premain - executes before main
+ *
+ * Description: Prints specific sentences before main execution
  */
-void __attribute__((constructor)) premain()
+void __attribute__((constructor)) premain(void)
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
 }
+
