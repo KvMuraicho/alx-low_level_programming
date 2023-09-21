1bd20f2c9af12a941b280a62e26fc632f53c1a32
KvMuraicho <muraichokelly@gmail.com>
2023-09-21 08:29:12 -0700

    created 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**
- * premain - executes before main
- *
- * Description: Prints specific sentences before main execution
- */
-void __attribute__((constructor)) premain(void)
-{
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
-}
+void __attribute__((constructor)) hare(void);
 
+/**
+ * hare - Prints a string before the
+ *   main function is executed.
+ */
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
