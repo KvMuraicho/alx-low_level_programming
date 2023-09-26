bc36973a286b98bbbd8e2fa52a063435838fba43
KvMuraicho <muraichokelly@gmail.com>
2023-09-26 14:36:44 -0700

    13

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,43 +3,70 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list.
- * Return: number of nodes in the list.
+ * find_loop_start - Finds the starting node of a loop.
+ * @head: Pointer to the head of the list.
+ * @meeting: Point where hare and tortoise pointers met.
+ * Return: Pointer to the start of the loop, or NULL if no loop.
+ */
+const listint_t *find_loop_start(const listint_t *head, const listint_t *meeting)
+{
+	while (head != meeting)
+	{
+		head = head->next;
+		meeting = meeting->next;
+	}
+	return (head);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the start of the list.
+ * Return: Number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t nodes = 0;
-    const listint_t *slow, *fast, *start = NULL, *current;
+	const listint_t *tortoise = head, *hare = head;
+	const listint_t *loop_start = NULL;
+	size_t nodes = 0;
 
-    if (!head)
-        exit(98);
+	if (!head)
+		exit(98);
 
-    slow = head;
-    fast = head;
+	while (hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        nodes++;
+		if (tortoise == hare)
+		{
+			loop_start = find_loop_start(head, hare);
+			break;
+		}
+	}
 
-        if (slow == fast)
-        {
-            start = slow;
-            break;
-        }
-    }
+	tortoise = head;
+	while (tortoise && tortoise != loop_start)
+	{
+		nodes++;
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		tortoise = tortoise->next;
+	}
 
-    current = head;
-    while (current && (!start || current != start->next))
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        current = current->next;
-    }
+	if (loop_start)
+	{
+		do
+		{
+			nodes++;
+			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+			tortoise = tortoise->next;
 
-    if (start)
-        printf("-> [%p] %d\n", (void *)start, start->n);
+			if (tortoise == loop_start)
+			{
+				printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			}
+		} while (tortoise != loop_start);
+	}
 
-    return nodes;
+	return (nodes);
 }
+
