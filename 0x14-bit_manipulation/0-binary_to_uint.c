1ed2c9027ff2a44189f3000af68b65a19b606513
KvMuraicho <muraichokelly@gmail.com>
2023-09-28 02:23:38 -0700

    binary to unint betty fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,38 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string of binary number
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of binary number.
  *
- * Return: the converted number, or
+ * Return: The converted number, or
  *         0 if there is one or more chars in the string b that is not 0 or 1
  *         or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int dec_val = 0;
+	unsigned int dec_val = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+	{
+		return (0);
+	}
 
-    while (*b)
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
 
-        dec_val <<= 1;
+		dec_val <<= 1;
 
-        if (*b == '1')
-            dec_val += 1;
+		if (*b == '1')
+		{
+			dec_val += 1;
+		}
 
-        b++;
-    }
+		b++;
+	}
 
-    return (dec_val);
+	return (dec_val);
 }
