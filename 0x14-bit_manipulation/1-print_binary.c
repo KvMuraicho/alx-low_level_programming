6dc3a8c7bdadfa8268039578f73e5292dee89bb3
KvMuraicho <muraichokelly@gmail.com>
2023-09-28 02:30:42 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,27 +3,29 @@
 void print_bits(unsigned long int n);
 
 /**
- * print_binary - prints the binary representation of a number.
- * @n: the number.
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number.
  */
 void print_binary(unsigned long int n)
 {
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-    print_bits(n);
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_bits(n);
 }
 
 /**
- * print_bits - recursively prints the binary representation of a number.
- * @n: the number.
+ * print_bits - Recursively prints the binary representation of a number.
+ * @n: The number.
  */
 void print_bits(unsigned long int n)
 {
-    if (n == 0)
-        return;
-    print_bits(n >> 1);
-    _putchar((n & 1) + '0');
+	if (n == 0)
+	{
+		return;
+	}
+	print_bits(n >> 1);
+	_putchar((n & 1) + '0');
 }
